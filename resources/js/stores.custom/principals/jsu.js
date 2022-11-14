17fdaa394db6b7e3362057503da5810e55d1520f
Kaloy <superloika25@gmail.com>
2022-11-14 10:14:28 +0800

    JSU template format adjustment

--- resources/js/stores.custom/principals/jsu.js
+++ resources/js/stores.custom/principals/jsu.js
@@ -1,20 +1,6 @@
 import Vue from "vue";
 
 const state = Vue.observable({
-    customersTableHeader: [
-        [
-            { text: "Customer Code", value: "customer_code" },
-            { text: "Customer Code (Supplier)", value: "customer_code_supplier" },
-            { text: "Name", value: "customer_name" },
-        ],
-    ],
-    itemsTableHeader: [
-        [
-            {text:"Item Code", value:"item_code"},
-            {text:"Item Code (Supplier)", value:"item_code_supplier"},
-            {text:"Description (Supplier)", value:"description_supplier"},
-        ]
-    ],
 
     // templated data table header
     generatedDataTableHeader: [
@@ -24,74 +10,28 @@ const state = Vue.observable({
             {text:"Customer Name", value: "customer_name"},
             {text:"Invoice Date (M/D/Y)", value: "invoice_date"},
             {text:"Item Code (NAV)", value: "alturas_item_code"},
-            // {text:"Item Code (Supplier)", value: "item_code"},
-            {text:"Item Name (NAV)", value: "description"},
-            // {text:"Item Name (Supplier)", value: "description_supplier"},
+            {text:"Item Code (Supplier)", value: "item_code"},
+            {text:"Item Name (NAV)", value: "item_description"},
+            {text:"Item Name (Supplier)", value: "description_supplier"},
+            {text:"UOM", value: "uom"},
             {text:"Quantity", value: "quantity"},
-            // {text:"Bulk Quantity", value: "bulk_qty"},
-            // {text:"Loose Quantity", value: "loose_qty"},
             {text:"Price", value: "price"},
             {text:"Amount", value: "amount"},
-            {text:"UOM", value: "uom"},
-            // {text:"Base UOM", value: "base_uom"},
-            // {text:"Conversion UOM", value: "uom"},
-            {text:"Salesman Name", value: "sm_name"},
+            {text:"Salesman", value: "sm_code"},
+            {text:"Group", value: "group"}
         ],
     ],
-
-    // transactions table header
-    transactionsTableHeader: [
-        [
-            {text:"Upload Date", value:"updated_at"},
-            {text:"Customer Code", value:"customer_code"},
-            {text:"Account Name", value:"customer_name"},
-            {text:"Sales Invoice", value:"doc_no"},
-            {text:"Item Code", value:"item_code"},
-            {text:"Description", value:"description"},
-            {text:"UOM", value:"uom"},
-            {text:"Quantity", value:"quantity"},
-            {text:"Amount", value:"u3"},
-        ]
-    ],
-
 
     // ***********************************************************************************
     generatedDataHistoryFilters: [
         [
-            {text: 'Generated Date', value: 'generated_at'},
+            {text: 'System Date', value: 'system_date'},
             {text: 'Item Code', value: 'item_code'},
             {text: 'Customer Code', value: 'customer_code'},
+            {text: 'Source Group', value: 'group_code'},
         ]
     ],
-    generatedDataDBTableColumns: [
-        // common
-        'id',
-        'principal_code',
-        'template_variation',
-        'generated_at',
-        'uploaded_by',
-        'status',
-        'doc_no',
-        // principal template
-        'invoice_no',
-        'customer_code',
-        'alturas_customer_code',
-        'customer_name',
-        'invoice_date',
-        'alturas_item_code',
-        'item_code',
-        'description',
-        'description_supplier',
-        'quantity',
-        // 'bulk_qty',
-        // 'loose_qty',
-        'price',
-        'amount',
-        'base_uom',
-        'uom',
-        'sm_name',
-        'system_date'
-    ],
+
 });
 
 
