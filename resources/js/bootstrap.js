777d76db017e1897e189630c9f93e63cc8e2a6f9
Kaloy <superloika25@gmail.com>
2023-06-29 09:30:06 +0800

    devchat

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -75,16 +75,16 @@ import Echo from 'laravel-echo';
 
 window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     // forceTLS: true,
-//     forceTLS: false,
-//     wsHost: window.location.hostname,
-//     wsPort: 6001,
-//     disableStats: true,
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    // forceTLS: true,
+    forceTLS: false,
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    disableStats: true,
+});
 
 
 // check if user session has expired or user is unauthorized
