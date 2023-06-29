d555a6c8e948eae216c5920bd3d21bbe3184a638
Kaloy <superloika25@gmail.com>
2023-06-29 11:38:22 +0800

    pusher port 6002

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,9 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                // 'useTLS' => true,
+                'useTLS' => false,
                 'host' => '127.0.0.1',
-                'port' => '6001',
+                'port' => '6002',
                 'scheme' => 'http',
             ],
         ],
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -82,7 +82,7 @@ window.Echo = new Echo({
     // forceTLS: true,
     forceTLS: false,
     wsHost: window.location.hostname,
-    wsPort: 6001,
+    wsPort: 6002,
     disableStats: true,
 });
 
