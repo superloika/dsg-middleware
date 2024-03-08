dd6ab4d9b0eabe2a62f85f21e9cd873f2b37e2c9
Kaloy <superloika25@gmail.com>
2024-03-08 14:13:40 +0800

    Echo config minor mod

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -51,10 +51,8 @@ import Echo from 'laravel-echo';
 window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    // key: process.env.MIX_PUSHER_APP_KEY,
-    // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    key: 'dsg_middleware_stage',
-    cluster: 'ap1',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     // forceTLS: true,
     forceTLS: false,
     wsHost: window.location.hostname,
